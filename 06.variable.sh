7ae47b636dc732653cf0f6a891a7324b6d6beacb
tanuja9346 <tanujach9346@gmail.com>
2023-08-20 16:49:49 +0530

    variable add

---
+++ 06.variable.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "please enter  your username"
+
+
+read -s USERNAME  # This is the variable where the value of the one user eneterd and read command is for it takes input from terminal. 
+
+#this is for validation purpose, you should not print in the terminal
+
+echo "username enetered is: $USERNAME"
+
+echo "please enter  your password"
+
+read -s PASSWORD   #this is the variable where the value of the one user eneterd
+
+#this is for validation purpose, you should not print in the terminal
+
+echo "Password entered is: $PASSWORD"
