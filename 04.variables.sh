591fbc7e1a524167a4511bbc290fa5cea7fe5f4e
tanuja9346 <tanujach9346@gmail.com>
2023-08-20 15:46:49 +0530

    variable add

---
+++ 04.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#this is comment, we need to develop a program that can do a addition of 2 numbers.
+
+NUMBER1=$1   # take input from user and declare variable i.e number1
+NUMBER2=$2
+# i need to run a command inside the shell and these 2 numbers
+ SUM=$((NUMBER1+NUMBER2))   # here variable is SUM
+
+ echo "Addition of 2 numbers is: $SUM"
