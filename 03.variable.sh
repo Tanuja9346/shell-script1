7568086fcd600b5c7299f97e6c25b4facb737776
tanuja9346 <tanujach9346@gmail.com>
2023-08-20 15:24:02 +0530

    variable add

---
+++ 03.variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hi hello world i am learning devOps with AWS"
+person1=$1 #this is the first value from command line.
+person2=$2 #this is the second value from command line.
+
+echo "$person1: Hi $person2, good morning"
+echo "$person2: Hi $person1, very good morning"
+echo "$person1: How are you $person2"
+echo "$person2: I am gud, how are $person1"
