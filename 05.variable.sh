a58623e8084c0805ca392a5b5a8971c79e5b4357
tanuja9346 <tanujach9346@gmail.com>
2023-08-20 16:40:35 +0530

    variable add

--- 05.variable.sh
+++ 05.variable.sh
@@ -2,6 +2,13 @@
 
 echo "please enter  your username"
 
+
 read USERNAME  # This is the variable where the value of the one user eneterd
 
 echo "username enetered is: $USERNAME"
+
+echo "please enter  your password"
+
+read PASSWORD   #this is the variable where the value of the one user eneterd
+
+echo "Password entered is: $PASSWORD"
