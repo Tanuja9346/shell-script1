87489cf44bd51be80f7c2c60234db9997ea1db38
tanuja9346 <tanujach9346@gmail.com>
2023-08-21 17:02:31 +0530

    add mysql

---
+++ 09.install-mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#our program goal is to install mysql
+USERID=$(id -u)
+
+if[ USERID -ne 0 ]
+then
+    echo "Error: please run this script with root access"
+# else
+    # echo "info: you are root user"
+fi
+
+  yum install mysql -y
