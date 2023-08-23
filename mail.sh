1763e8589e3de704ae4d4c997ab4cb8c5ae6ce70
tanuja9346 <tanujach9346@gmail.com>
2023-08-23 23:04:38 +0530

    add mail

--- mail.sh
+++ mail.sh
@@ -5,12 +5,13 @@
 
 TO_ADDRESS=$1
 SUBJECT=$2
-BODY=$3
+BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3)
+echo "escaped content: $BODY"
 TEAM_NAME=$4
 ALERT_TYPE=$5
 
 echo "all arguments: $@"
 
-FINAL_BODY=$(sed -e 's/Team_name/DevOps_team/g' -e 's/ALERT_TYPE/High disk usage/g' -e "s/MESSAGE/$BODY/g" template.html
+FINAL_BODY=$(sed -e "s/Team_name/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html
 
 echo "$FINAL_BODY" | mail -s "$SUBJECT" "$TO_ADDRESS"
