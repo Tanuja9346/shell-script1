7befd727902eedd4b7a311b10629a4302c52b05c
tanuja9346 <tanujach9346@gmail.com>
2023-08-28 15:52:41 +0530

    ec2 add

--- 19.ec2-instances.sh
+++ 19.ec2-instances.sh
@@ -4,7 +4,7 @@
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalouge" "user" "cart" "shipping" "payment" "dispatch" "web")
 
 
-for i in "{$NAMES[@]}"
+for i in "{$NAMES[$@]}"
 do
    echo "NAMES: $i"
 
