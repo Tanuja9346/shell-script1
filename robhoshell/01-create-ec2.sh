778db2f64c0d4efe77cac293cb3bb9e4a8a0e75d
tanuja9346 <tanujach9346@gmail.com>
2023-08-28 13:47:26 +0530

    add old logd

---
+++ robhoshell/01-create-ec2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalouge" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+
+for i in "{$NAMES[@]}"
+do
+   echo "NAMES: $i"
+
+done   
