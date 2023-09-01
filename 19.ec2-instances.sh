0a525d0e71dbae6ae792b324882f92e09ae52d41
tanuja9346 <tanujach9346@gmail.com>
2023-09-01 12:14:51 +0530

    ec2-instance add

--- 19.ec2-instances.sh
+++ 19.ec2-instances.sh
@@ -3,27 +3,23 @@
 
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalouge" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
-IMAGE_ID="ami-03265a0778a880afb"
-SECURITY_GROUP_ID="sg-0ac9f1b8ae2c47986"
-
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-0ac9f1b8ae2c47986
 
 #loop through the array.
 #if mysql or mongodb are instance-type should be t3.medium , for all others it is t2.micro.
 
 for i in "${NAMES[@]}"
 do
-    if  [[$i == "mongodb"] || [$i == "mysql"]]
+    if  [[ $i == "mongodb" || $i == "mysql" ]]
     then
        INSTANCE_TYPE="t3.medium"
     else
        INSTANCE_TYPE="t2.micro"
     fi
    echo "creating $i instance"
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '. Instances[0].PrivateIpAddress')
-  echo "creating $i instance: $IP_ADDRESS"
-
-
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '. Instances[0].PrivateIpAddress')
+  echo "created $i instance: $IP_ADDRESS"
+  
 #running above command line u will get blank instance so we dont know which instance is running so declare TAG SO U can tag before the instance or after creating instance.
-
-
-done   
+done  
